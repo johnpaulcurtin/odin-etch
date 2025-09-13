e617fbe3d26a2a3b3309f9a1f809748ef5a867d2
johnpaulcurtin <juanlove@gmail.com>
2025-09-12 18:03:52 -0700

    add eventlistener to divs add minor style changes

--- script.js
+++ script.js
@@ -14,20 +14,30 @@ function gridCreator(x) {
     const containerHeight = 40 + (40 * x);
     container.style.height=`${containerHeight}px`;
     container.style.width=`${containerHeight}px`
-    console.log(containerHeight);
+
 
     for(i = 0; i < x * x; i++){
     const box = document.createElement('div');
     box.classList.add('divJS');
     const flexB = 100/x
     box.style.flexBasis=`${flexB}%`;
-
-    
+   box.addEventListener('mouseover', () => {
+      box.style.backgroundColor = '#62d6d6a4';
+      }, .5);
     container.appendChild(box);
-    }
-  }
+    };
+};
 
-gridCreator(16); 
+
+gridCreator(4); 
+
+
+
+//event listen for hover to change div background color to a darker blue
+
+
+
+
 
 
 
